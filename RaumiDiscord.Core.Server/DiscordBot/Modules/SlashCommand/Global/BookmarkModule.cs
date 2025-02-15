bf04a58716a0673737677f52639ac55d9fa714d2
raumigit <86038378+raumigit@users.noreply.github.com>
2025-02-16 01:34:52 +0900

    fix BookmarkModule.cs

--- RaumiDiscord.Core.Server/DiscordBot/Modules/SlashCommand/Global/BookmarkModule.cs
+++ RaumiDiscord.Core.Server/DiscordBot/Modules/SlashCommand/Global/BookmarkModule.cs
@@ -2,15 +2,19 @@
 using Microsoft.EntityFrameworkCore;
 using RaumiDiscord.Core.Server.Api.Models;
 using RaumiDiscord.Core.Server.DataContext;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace RaumiDiscord.Core.Server.DiscordBot.Modules.SlashCommand.Global
 {
     public class BookmarkModule : InteractionModuleBase<SocketInteractionContext>
     {
+        private readonly LoggingService LoggingService;
         private readonly DeltaRaumiDbContext deltaRaumiDb;
-        public BookmarkModule(DeltaRaumiDbContext deltaRaumiDb)
+        public BookmarkModule(DeltaRaumiDbContext deltaRaumiDb,LoggingService logger)
         {
             this.deltaRaumiDb = deltaRaumiDb;
+            this.LoggingService = logger;
         }
 
         [SlashCommand("hoyocode", "HoYoverseで使えるギフトコードを出力します")]
@@ -20,9 +24,9 @@ namespace RaumiDiscord.Core.Server.DiscordBot.Modules.SlashCommand.Global
             [Choice("Set","set")]
             string action,
             [Summary("type","URLのタイプを指定")]
-            [Choice("URL","url")]
+            [Choice("URL","URL")]
             [Choice("GenshinImpact","GI")]
-            [Choice("HonkaiSterrail","HSR")]
+            [Choice("HonkaiStarRail","HSR")]
             [Choice("ZenlessZoneZero","ZZZ")]
             string urlType,
             string url = null,
@@ -35,13 +39,44 @@ namespace RaumiDiscord.Core.Server.DiscordBot.Modules.SlashCommand.Global
                     await RespondAsync("URLと有効期限を指定してください。(有効期限：yyyy/mm/dd-hh:mm:sszzz)", ephemeral: true);
                     return;
                 }
-                if (!DateTimeOffset.TryParseExact(ttl, "yyyy/MM/dd-HH:mm:sszzz", null, System.Globalization.DateTimeStyles.None, out DateTimeOffset expirationTime))
+                if (!DateTimeOffset.TryParseExact(ttl, "yyyy/MM/dd-HH:mm:sszzz", null, System.Globalization.DateTimeStyles.None, out DateTimeOffset expirationTime) && expirationTime.UtcDateTime <= DateTime.UtcNow)
                 {
-                    await RespondAsync("TTLの形式が正しくありません。yyyy/MM/dd-HH:mm:sszzz 形式で入力してください。", ephemeral: true);
+                    await RespondAsync("TTLの形式が正しくありません。yyyy/MM/dd-HH:mm:sszzz 形式で入力してください。\n" +
+                        "または有効ではない時間を入力している可能性があります。", ephemeral: true);
                     return;
                 }
+                if (urlType == "URL")
+                {
+                    if (!url.StartsWith("https://") && !url.StartsWith("http://"))
+                    {
+                        await RespondAsync("URLが正しくありません。", ephemeral: true);
+                        return;
+                    }
+                }
+                else if (urlType == "GI" || urlType == "HSR" || urlType == "ZZZ")
+                {
+                    string baseUrl = urlType switch
+                    {
+                        "GI" => "https://genshin.hoyoverse.com/ja/gift?code=",
+                        "HSR" => "https://hsr.hoyoverse.com/gift?code=",
+                        "ZZZ" => "https://zzz.hoyoverse.com/gift?code=",
+                        _ => ""
+                    };
 
+                    if (url.StartsWith(baseUrl))
+                    {
+                        Console.WriteLine($"URL->{url}");
+                    }
+
+                    if (Regex.IsMatch(url, "^[A-Z0-9]+$"))
+                    {
+                        url = baseUrl + url;
+                    }
+                    await LoggingService.LogGeneral($"URLがCodeだったため修正しました。");
+                }
                 
+
+
 
                 // UnixTime 形式に変換
                 long unixExpirationTime = expirationTime.ToUnixTimeSeconds();
@@ -52,6 +87,13 @@ namespace RaumiDiscord.Core.Server.DiscordBot.Modules.SlashCommand.Global
                     UrlType = urlType,
                     TTL = expirationTime.UtcDateTime
                 };
+
+                var Url_record = deltaRaumiDb.UrlDetaModels.Where(k => k.Url == url).ToList();
+                if (Url_record.Any())
+                {
+                    await RespondAsync("既に登録されています。",ephemeral:true);
+                    return;
+                }
                 deltaRaumiDb.UrlDetaModels.Add(newEntry);
                 await deltaRaumiDb.SaveChangesAsync();
                 await RespondAsync($"登録完了: {urlType} - {url} (期限: <t:{unixExpirationTime}:R>)");
--- RaumiDiscord.Core.Server/DiscordBot/Services/ComponentInteractionService.cs
+++ RaumiDiscord.Core.Server/DiscordBot/Services/ComponentInteractionService.cs
@@ -113,7 +113,7 @@ namespace RaumiDiscord.Core.Server.DiscordBot.Services
                             builder.WithAuthor(component.User);
                             builder.WithTitle("サーバーの状態");
                             builder.WithDescription(
-                                "バージョン：0.1.1.3 (2025/02/13-22:42)\n " +
+                                "バージョン：0.1.1.5 (2025/02/14-18:35)\n " +
                                 "外部連携：null\n" +
                                 "読み上げエンジン：null\n" +
                                 "WebGUI：null" +
