bb0b0d7cda80bfdcf24361a4a570e8d4e2f61a1f
raumigit <86038378+raumigit@users.noreply.github.com>
2025-02-22 03:19:43 +0900

    web の修正(ページの有効化)

--- RaumiDiscord.Core.Server/Program.cs
+++ RaumiDiscord.Core.Server/Program.cs
@@ -29,16 +29,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI(options =>
-//    {
-//        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-//        options.RoutePrefix = string.Empty;
-//    });
-//}
+
+if (app.Environment.IsDevelopment())
+{
+    
+}
 
 app.UseSwagger();
 app.UseSwaggerUI(options =>
